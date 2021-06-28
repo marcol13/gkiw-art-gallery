cc30bcfc969091ac730c2c6866c87114bae8a743
mackurzawa <mac.kurzawa@gmail.com>
2021-06-28 15:08:02 +0200

    Shaders update

--- Shaders/f_simplest.glsl
+++ Shaders/f_simplest.glsl
@@ -1,32 +1,14 @@
 ﻿#version 330
 
-uniform sampler2D textureMap0;
-uniform sampler2D textureMap1;
+uniform sampler2D textureMap0; //zmienna reprezentująca jednostkę teksturującą
 
 out vec4 pixelColor; //Zmienna wyjsciowa fragment shadera. Zapisuje sie do niej ostateczny (prawie) kolor piksela
 
+in vec2 iTexCoord0; //Zmienna do interpolacji współrzędnych teksturowania
 in vec4 ic; 
-in vec4 n;
-in vec4 l;
-in vec4 v;
-in vec2 iTexCoord0;
-in vec2 iTexCoord1;
 
 void main(void) {
+	vec4 texColor=texture(textureMap0,iTexCoord0); //Pobranie koloru
 
-	//Znormalizowane interpolowane wektory
-	vec4 ml = normalize(l);
-	vec4 mn = normalize(n);
-	vec4 mv = normalize(v);
-	//Wektor odbity
-	vec4 mr = reflect(-ml, mn);
-
-	//Parametry powierzchni
-	vec4 kd = mix(texture(textureMap0,iTexCoord0),texture(textureMap1, iTexCoord1),0.3);
-	vec4 ks = vec4(1,1,1,1);
-
-	//Obliczenie modelu oświetlenia
-	float nl = clamp(dot(mn, ml), 0, 1);
-	float rv = pow(clamp(dot(mr, mv), 0, 1), 50);
-	pixelColor= vec4(kd.rgb * nl, kd.a) + vec4(ks.rgb*rv, 0);
+	pixelColor = ic;
 }
--- Shaders/v_simplest.glsl
+++ Shaders/v_simplest.glsl
@@ -8,25 +8,18 @@ uniform mat4 M;
 //Atrybuty
 in vec4 vertex; //wspolrzedne wierzcholka w przestrzeni modelu
 in vec4 color; //kolor związany z wierzchołkiem
-in vec4 normal; //wektor normalny w przestrzeni modelu
-in vec2 texCoord0;
+in vec2 texCoord0; //Atrybut do przekazania współrzędnych teksturowania
+
 
 //Zmienne interpolowane
 out vec4 ic;
-out vec4 l;
-out vec4 n;
-out vec4 v;
-out vec2 iTexCoord0;
-out vec2 iTexCoord1;
+out vec2 iTexCoord0; //Zmienna do interpolacji współrzędnych teksturowania
 
 void main(void) {
-    vec4 lp = vec4(0, 0, -6, 1); //przestrzeń świata
-    l = normalize(V * lp - V*M*vertex); //wektor do światła w przestrzeni oka
-    v = normalize(vec4(0, 0, 0, 1) - V * M * vertex); //wektor do obserwatora w przestrzeni oka
-    n = normalize(V * M * normal); //wektor normalny w przestrzeni oka
-    iTexCoord0 = texCoord0;
-    iTexCoord1 = (n.xy + 1) / 2;
-    ic = color;
+    iTexCoord0=texCoord0; //Przepisanie danych z atrybutu do zmiennej interpolowanej
+    float d = distance(V*M*vertex, V*M*vec4(0, 0, 20, 1));
+    d = 1 - (d - 2)/40;
+    ic = vec4(color.rgb*d, color.a);
     
     gl_Position=P*V*M*vertex;
 }
