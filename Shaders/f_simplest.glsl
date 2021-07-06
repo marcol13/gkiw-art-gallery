34b68f3390add852bb1912b45493cd214cf8aa53
mackurzawa <mac.kurzawa@gmail.com>
2021-07-07 01:25:28 +0200

    Big Frame fixed

--- Shaders/f_simplest.glsl
+++ Shaders/f_simplest.glsl
@@ -26,7 +26,7 @@ void main(void) {
 	//specular
 	float specularStrength = 0.5;
 	vec3 viewDir = normalize(viewPos - FragPos);
-    vec3 reflectDir = normalize(reflect(-lightDir, norm));  
+    vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
     vec3 specular = specularStrength * spec * vec3(1.0, 1.0, 1.0);
 
