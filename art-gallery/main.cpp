e2281ac0b3d4ea5d0198d6d4017ff6dc5ef29f50
Marcin <marcinkrueger@onet.eu>
2021-07-07 22:40:09 +0200

    add sculptures

--- art-gallery/main.cpp
+++ art-gallery/main.cpp
@@ -52,7 +52,11 @@ Model* EddieHall;
 Model* HallThor;
 Model* Dome1;
 Model* Dome2;
+
 Model* venus;
+Model* discobolus;
+Model* nimph;
+
 Model* pedestal_venus;
 Model* pedestal_venus2;
 Model* pedestal_venus3;
@@ -67,7 +71,11 @@ Model* column1;
 Model* column2;
 Model* column3;
 Model* column4;
+
 Model* mask;
+Model* thanos;
+Model* napoleon;
+Model* ok_sculpture;
 
 Model* frame1_v1;
 Model* frame2_v1;
@@ -79,9 +87,12 @@ Model* frame7_v1;
 Model* frame8_v1;
 Model* frame9_v1;
 Model* frame10_v1;
+Model* frame11_v1;
+Model* frame12_v1;
 
 Model* frame1_v2;
 Model* frame2_v2;
+Model* frame3_v2;
 
 Model* canvas0_supper;
 Model* canvas1_stanczyk;
@@ -94,6 +105,10 @@ Model* canvas7_noc;
 Model* canvas8_noc;
 Model* canvas9_noc;
 Model* canvas10_noc;
+Model* canvas11_noc;
+Model* canvas12_noc;
+
+Model* canvas1_lasiczka;
 
 
 
@@ -171,6 +186,16 @@ void initOpenGLProgram(GLFWwindow* window) {
 	venus->translate(glm::vec3(-8, 3.2, -9));
 	venus->scale(glm::vec3(1.7, 1.7, 1.7));
 
+	discobolus = new Model("Discobolus1", "Marble010", "simplest");
+	discobolus->scale(glm::vec3(0.35,0.35,0.35));
+	discobolus->translate(glm::vec3(-22,9.5,206));
+	discobolus->rotate(180.0f * PI / 180.0f, glm::vec3(0, 1, 0));
+
+	nimph = new Model("Nimph", "Marble010", "simplest");
+	nimph->rotate(270.0f * PI / 180.0f, glm::vec3(0, 1, 0));
+	nimph->translate(glm::vec3( 3, 7.5, -16.2));
+	nimph->scale(glm::vec3(1.2, 1.2, 1.2));
+
 	pedestal_venus = new Model("Pedestal2", "Marble019", "simplest");
 	pedestal_venus->scale(glm::vec3(0.3, 0.25, 0.3));
 	pedestal_venus->translate(glm::vec3(-26, 6.85, -30));
@@ -223,8 +248,6 @@ void initOpenGLProgram(GLFWwindow* window) {
 	big_frame1->rotate(90.0f * PI / 180.0f, glm::vec3(0, 1, 0));
 	big_frame1->rotate(90.0f * PI / 180.0f, glm::vec3(1, 0, 0));
 	big_frame1->scale(glm::vec3(13,13,17));
-	//big_frame1->scale(glm::vec3(10, 1, 1));
-	//big_frame1->translate(glm::vec3(1.5, 1.65, 0.65));
 	big_frame1->translate(glm::vec3(1.35, 3.97, 0.5));
 
 	floor1 = new Model("Floor", "Tiles074", "simplest");
@@ -250,7 +273,6 @@ void initOpenGLProgram(GLFWwindow* window) {
 	frame1_v1 = new Model("Frame2", "Planks011", "simplest");
 	frame1_v1->scale(glm::vec3(0.5, 0.5, 0.5));
 	frame1_v1->rotate(90.0f * PI / 180.0f, glm::vec3(0, 1, 0));
-	//frame1_v1->translate(glm::vec3(-54, 20, 65));
 	frame1_v1->translate(glm::vec3(-53.9, 25, 80));
 
 	frame2_v1 = new Model("Frame2", "Planks011", "simplest");
@@ -292,6 +314,14 @@ void initOpenGLProgram(GLFWwindow* window) {
 	frame10_v1->scale(glm::vec3(0.5, 0.5, 0.5));
 	frame10_v1->translate(glm::vec3(-26.15, 15, 88));
 
+	frame11_v1 = new Model("Frame2", "Planks011", "simplest");
+	frame11_v1->scale(glm::vec3(0.5, 0.5, 0.5));
+	frame11_v1->translate(glm::vec3(-46.15, 18, 144));
+
+	frame12_v1 = new Model("Frame2", "Planks011", "simplest");
+	frame12_v1->scale(glm::vec3(0.5, 0.5, 0.5));
+	frame12_v1->translate(glm::vec3(16.15, 18, 144));
+
 
 	frame1_v2 = new Model("Frame2", "Planks011", "simplest");
 	frame1_v2->scale(glm::vec3(0.5, 0.5, 0.5));
@@ -303,6 +333,12 @@ void initOpenGLProgram(GLFWwindow* window) {
 	frame2_v2->rotate(90.0f * PI / 180.0f, glm::vec3(1, 0, 0));
 	frame2_v2->rotate(90.0f * PI / 180.0f, glm::vec3(0, 0, 1));
 	frame2_v2->translate(glm::vec3(-48.5,21,-13));
+
+	frame3_v2 = new Model("Frame2", "Planks011", "simplest");
+	frame3_v2->scale(glm::vec3(0.5, 0.5, 0.5));
+	frame3_v2->rotate(90.0f * PI / 180.0f, glm::vec3(1, 0, 0));
+	frame3_v2->rotate(90.0f * PI / 180.0f, glm::vec3(0, 0, 1));
+	frame3_v2->translate(glm::vec3(175, 21, -13));
 	
 
 	canvas0_supper = new Model("BigPainting", "LastSupper", "simplest");
@@ -365,10 +401,43 @@ void initOpenGLProgram(GLFWwindow* window) {
 	canvas10_noc->translate(glm::vec3(-42.8, 13.3, 5));
 	canvas10_noc->scale(glm::vec3(2.55, 2.5, 2.5));
 
+	canvas11_noc = new Model("Stanczyk", "GwiezdzistaNoc", "simplest");
+	canvas11_noc->rotate(90.0f * PI / 180.0f, glm::vec3(-1, 0, 0));
+	canvas11_noc->rotate(90.0f * PI / 180.0f, glm::vec3(0, 0, 1));
+	canvas11_noc->translate(glm::vec3(-70.8, 23.3, 6.5));
+	canvas11_noc->scale(glm::vec3(2.55, 2.5, 2.5));
+
+	canvas12_noc = new Model("Stanczyk", "GwiezdzistaNoc", "simplest");
+	canvas12_noc->rotate(90.0f * PI / 180.0f, glm::vec3(-1, 0, 0));
+	canvas12_noc->rotate(90.0f * PI / 180.0f, glm::vec3(0, 0, 1));
+	canvas12_noc->translate(glm::vec3(-70.8, -7.9, 6.5));
+	canvas12_noc->scale(glm::vec3(2.55, 2.5, 2.5));
+
+	canvas1_lasiczka = new Model("Dama", "DamaZLasiczka", "simplest");
+	canvas1_lasiczka->rotate(90.0f * PI / 180.0f, glm::vec3(-1, 0, 0));
+	canvas1_lasiczka->translate(glm::vec3(-7.87, 24.3, 6.62));
+	canvas1_lasiczka->scale(glm::vec3(2.02, 2.1, 2.27));
+
 	mask = new Model("Mask", "Mask", "simplest");
 	mask->translate(glm::vec3(4.65, 2.21, 6.7));
 	mask->rotate(150.0f * PI / 180.0f, glm::vec3(0, -1, 0));
 	mask->scale(glm::vec3(0.8, 0.8, 0.8));
+
+	thanos = new Model("Thanos", "Marble010", "simplest");
+	thanos->scale(glm::vec3(0.4, 0.4, 0.4));
+	thanos->rotate(45.0f * PI / 180.0f, glm::vec3(0, 1, 0));
+	thanos->translate(glm::vec3(2.4, 4.8, -66));
+
+	napoleon = new Model("Napoleon", "Marble010", "simplest");
+	napoleon->scale(glm::vec3(0.7, 0.7, 0.7));
+	napoleon->rotate(45.0f * PI / 180.0f, glm::vec3(0, -1, 0));
+	napoleon->translate(glm::vec3(-17.2, 6.65, -21.5));
+	napoleon->scale(glm::vec3(0.8, 0.8, 0.8));
+
+	ok_sculpture = new Model("Ok", "Marble010", "simplest");
+	ok_sculpture->scale(glm::vec3(0.08, 0.08, 0.08));
+	ok_sculpture->rotate(200.0f * PI / 180.0f, glm::vec3(0, 1, 0));
+	ok_sculpture->translate(glm::vec3(236, 46.8, 38.8));
 
 	swiatlo = new Model("LightsourceCube", "bricks066", "lightsource");
 	swiatlo->translate(swiatlo->pointLightPositions[0]);
@@ -405,7 +474,11 @@ void drawScene(GLFWwindow* window) {
 	HallThor->draw();
 	Dome1->draw();
 	Dome2->draw();
+
 	venus->draw();
+	discobolus->draw();
+	nimph->draw();
+
 	pedestal_venus->draw();
 	pedestal_venus2->draw();
 	pedestal_venus3->draw();
@@ -431,9 +504,12 @@ void drawScene(GLFWwindow* window) {
 	frame8_v1->draw();
 	frame9_v1->draw();
 	frame10_v1->draw();
+	frame11_v1->draw();
+	frame12_v1->draw();
 
 	frame1_v2->draw();
 	frame2_v2->draw();
+	frame3_v2->draw();
 
 	canvas0_supper->draw();
 	canvas1_stanczyk->draw();
@@ -446,8 +522,15 @@ void drawScene(GLFWwindow* window) {
 	canvas8_noc->draw();
 	canvas9_noc->draw();
 	canvas10_noc->draw();
+	canvas11_noc->draw();
+	canvas12_noc->draw();
+
+	canvas1_lasiczka->draw();
 
 	mask->draw();
+	thanos->draw();
+	napoleon->draw();
+	ok_sculpture->draw();
 
 	swiatlo->draw();
 	swiatlo2->draw();
